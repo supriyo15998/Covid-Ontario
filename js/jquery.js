d36e16743a53778ebbca44977ae8aab2bfa42b7d
supriyo15998 <supriyo15998@gmail.com>
2021-06-15 08:58:17 +0530

    initial

--- js/jquery.js
+++ js/jquery.js
@@ -12,7 +12,7 @@ $(document).ready(() => {
     
     $("#loadstrg").on("click",function() {
         $.ajax({
-            url: '../json/vaccine_doses.json',
+            url: 'json/vaccine_doses.json',
             method: "get",
             contentType: "jsonp",
             success: storeJson,
